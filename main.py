08646b23c95ba53d791a4915d5566482f7b92d91
Matheus Malta <matheusmalta.pessoal@gmail.com>
2025-03-02 02:12:26 -0300

    Detecta a face da pessoa e desenha um losango branco ao redor do seu rosto

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import cv2
+import numpy as np
 
-cascade = cv2.CascadeClassifier("haarcascades\haarcascade_frontalface_default.xml")
+cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 foto_grupo = cv2.imread("test/img/foto-grupo-pessoas-teste.jpg")
 foto_unica = cv2.imread("test/img/foto-teste.jpg")
+foto = foto_unica
 
-foto_cinza = cv2.cvtColor(foto_unica, cv2.COLOR_BGR2GRAY)
-faces = cascade.detectMultiScale(foto_cinza)
+foto_cinza = cv2.cvtColor(foto, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Faces", foto_unica) # Mostrando a foto na tela
-cv2.waitKey()
+faces = cascade.detectMultiScale(foto_cinza, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+for (x, y, w, h) in faces:
+    centro_x, centro_y = x + w // 2, y + h // 2
+
+    distancia = int(max(w, h) * 0.25)  
+
+    pontos = np.array([
+        (centro_x, y - distancia),
+        (x + w + distancia, centro_y),
+        (centro_x, y + h + distancia),
+        (x - distancia, centro_y)
+    ], np.int32)
+
+    cv2.polylines(foto, [pontos], isClosed=True, color=(255, 255, 255), thickness=2)
+
+cv2.imshow("Faces", foto)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
