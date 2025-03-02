3487764d49f78d76b0e37192064ca9c9ee235ee9
Matheus Malta <matheusmalta.pessoal@gmail.com>
2025-03-02 02:02:36 -0300

    Primeiros testes com openCV

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import cv2
 
 cascade = cv2.CascadeClassifier("haarcascades\haarcascade_frontalface_default.xml")
+foto_grupo = cv2.imread("test/img/foto-grupo-pessoas-teste.jpg")
+foto_unica = cv2.imread("test/img/foto-teste.jpg")
+
+foto_cinza = cv2.cvtColor(foto_unica, cv2.COLOR_BGR2GRAY)
+faces = cascade.detectMultiScale(foto_cinza)
+
+cv2.imshow("Faces", foto_unica) # Mostrando a foto na tela
+cv2.waitKey()
